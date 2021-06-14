a2944ac0e3146402d010d6ae0aeebde0b51a59fa
Vaisakh <ysakhpr@gmail.com>
2021-06-15 01:59:31 +0530

    ui changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { UserService } from './services/user.service'
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {HttpClientModule } from '@angular/common/http'
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 
 @NgModule({
@@ -20,7 +21,8 @@ import {HttpClientModule } from '@angular/common/http'
     AppRoutingModule,
     FormsModule,
     BrowserAnimationsModule,
-    HttpClientModule
+    HttpClientModule,
+    FlexLayoutModule
 
   ],
   providers: [UserService],
