7cb5f680ec245c68f7a5e3cd972e7811f8fdb946
Vaisakh <ysakhpr@gmail.com>
2021-06-19 14:45:13 +0530

    Select option validations and placeholder

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,39 +13,52 @@ import { Title } from '@angular/platform-browser';
 export class AppComponent {
   title = 'Vaccine notification';
 
-  alert :boolean = false
+  alert: boolean = false
+  ageGroupHasError = true;
   userModel = new User()
-  
 
-  constructor(private userService: UserService ,http :HttpClientModule, private titleService: Title){
+
+
+  constructor(private userService: UserService, http: HttpClientModule, private titleService: Title) {
     this.setTitle()
-      
+    this.userModel.ageGroup = 0;
   }
 
-  ngOnInit(){
+  ngOnInit() {
     this.setTitle
   }
 
-  public setTitle(){
+  public setTitle() {
     this.titleService.setTitle(this.title)
 
   }
 
-  registerUser(){
-    // console.log('Submitted');
+  registerUser() {
+    console.log('Submitted');
     // this.alert=true
-    this.userService.enroll(this.userModel).subscribe(
-      data => this.alert =true ,
-      error => this.alert=false
-    )
+    // this.userService.enroll(this.userModel).subscribe(
+    //   data => this.alert =true ,
+    //   error => this.alert=false
+    // )
   }
 
-  closeAlert(){
+  closeAlert() {
     this.alert = false
   }
 
-  onSubmit(){
+  onSubmit() {
     // console.log('Submitted');
+
+  }
+  validateAgeGroup(value: number) {
+
+    if (value == 0) {
+
+      this.ageGroupHasError = true;
+    } else {
+
+      this.ageGroupHasError = false;
+    }
 
   }
 }
